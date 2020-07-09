eff234a9211384ffbfa1d0c2aec9006ecf1c072a
Matt Martino <matt.martino@hey.com>
2020-07-08 23:17:25 -0400

    Initial Commit

---
+++ ParadoxCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParadoxCalculator
+//
+//  Created by Matt Martino on 7/8/20.
+//  Copyright © 2020 Matt Martino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
